cd68a45efcd7c0757f50a905a0321e776d5e8298
Vivian Santana <santanavivian245@gmail.com>
2025-10-24 02:00:11 -0300

    card medicos

---
+++ src/app/features/card-medicos/card-medicos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-card-medicos',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './card-medicos.component.html',
+  styleUrl: './card-medicos.component.css'
+})
+export class CardMedicosComponent {
+
+}
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CardConsultasComponent } from "../card-consultas/card-consultas.component";
 import { CardAgendarConsultaComponent } from '../card-agendar-consulta/card-agendar-consulta.component';
+import { CardMedicosComponent } from "../card-medicos/card-medicos.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, CardConsultasComponent, CardAgendarConsultaComponent],
+  imports: [RouterModule, CardConsultasComponent, CardAgendarConsultaComponent, 
+            CardMedicosComponent, CardMedicosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
