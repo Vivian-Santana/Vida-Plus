028f6f1272547373446520b7e4a497832f258233
Vivian Santana <santanavivian245@gmail.com>
2025-10-28 17:34:09 -0300

    criar environment.ts

--- set-env.js
+++ set-env.js
@@ -2,26 +2,36 @@ const fs = require('fs');
 const path = require('path');
 
 if (process.env.NETLIFY) {
-  const envDir = path.join(__dirname, 'src', 'environments');
-  const envPath = path.join(envDir, 'environment.prod.ts');
+    const envDir = path.join(__dirname, 'src', 'environments');
+    fs.mkdirSync(envDir, { recursive: true });
 
-  // Garante que a pasta existe
-  fs.mkdirSync(envDir, { recursive: true });
+    const apiUrl = process.env.API_URL;
 
-  const apiUrl = process.env.API_URL;
+    if (!apiUrl) {
+        throw new Error('API_URL não está definida no ambiente Netlify.');
+    }
 
-  if (!apiUrl) {
-    throw new Error('API_URL não está definida no ambiente Netlify.');
-  }
+    const prodPath = path.join(envDir, 'environment.prod.ts');
+    const devPath = path.join(envDir, 'environment.ts');
 
-  const targetEnv = `export const environment = {
+    const prodEnv = `export const environment = {
   production: true,
   apiUrl: "${apiUrl}"
 };
 `;
 
-  fs.writeFileSync(envPath, targetEnv, { encoding: 'utf8' });
-  console.log('Arquivo environment.prod.ts gerado');
+    const devEnv = `export const environment = {
+  production: false,
+  apiUrl: "${apiUrl}"
+};
+`;
+
+    fs.writeFileSync(prodPath, prodEnv, { encoding: 'utf8' });
+    fs.writeFileSync(devPath, devEnv, { encoding: 'utf8' });
+
+    console.log(`Arquivos gerados:
+    - ${prodPath}
+    - ${devPath}`);
 } else {
-  console.log('Ignorando set-env.js fora do Netlify');
+    console.log('Ignorando set-env.js fora do Netlify');
 }
