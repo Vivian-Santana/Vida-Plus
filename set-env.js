2be782985cf1af1356bfeeaf7938b2937288d315
Vivian Santana <santanavivian245@gmail.com>
2025-10-28 14:55:49 -0300

    Atualiza set-env.js para criar pasta environments

--- set-env.js
+++ set-env.js
@@ -1,11 +1,27 @@
 const fs = require('fs');
-const apiUrl = process.env.API_URL;
+const path = require('path');
 
-const envProd = `
-export const environment = {
+if (process.env.NETLIFY) {
+  const envDir = path.join(__dirname, 'src', 'environments');
+  const envPath = path.join(envDir, 'environment.prod.ts');
+
+  // Garante que a pasta existe
+  fs.mkdirSync(envDir, { recursive: true });
+
+  const apiUrl = process.env.API_URL;
+
+  if (!apiUrl) {
+    throw new Error('API_URL não está definida no ambiente Netlify.');
+  }
+
+  const targetEnv = `export const environment = {
   production: true,
-  apiUrl: '${apiUrl}'
+  apiUrl: "${apiUrl}"
 };
 `;
 
-fs.writeFileSync('./src/environments/environment.prod.ts', envProd);
+  fs.writeFileSync(envPath, targetEnv, { encoding: 'utf8' });
+  console.log('Arquivo environment.prod.ts gerado');
+} else {
+  console.log('Ignorando set-env.js fora do Netlify');
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./features/login/login.component";
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { NgIf } from '@angular/common';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -8,7 +7,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, NavbarComponent, NgIf , FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, NgIf, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
